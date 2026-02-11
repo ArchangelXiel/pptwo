8246e13aa603752738077bccc12b2313ca48e79d
Xiel <demonix2006@gmail.com>
2026-02-12 02:06:26 +0500

    Created file tree, W3Schools Functions and lambda completed

---
+++ Week3/functions/function_arguments.py
@@ -0,0 +1,19 @@
+def my_function(fname, lname):
+  print(fname + " " + lname)
+
+my_function("Emil", "Refsnes") 
+
+
+
+def my_function(fname, lname):
+  print(fname + " " + lname)
+
+my_function("Emil", "Refsnes") 
+
+
+
+def my_function(animal, name):
+  print("I have a", animal)
+  print("My", animal + "'s name is", name)
+
+my_function(name = "Buddy", animal = "dog") 

dbfd79480ce3f7aa576a7539df7502f23ea0432e
Xiel <demonix2006@gmail.com>
2026-02-12 14:37:14 +0500

    Completed W3schools Classes and inheritance

---
+++ Week3/classes/class_variables.py
@@ -0,0 +1,39 @@
+class Car:
+  def __init__(self, brand, model):
+    self.brand = brand
+    self.model = model
+
+car1 = Car("Toyota", "Corolla")
+
+print(car1.brand)
+print(car1.model) 
+
+
+
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("Linus", 30)
+
+del p1.age
+
+print(p1.name)
+
+
+
+class Person:
+  species = "Human" # Class property
+
+  def __init__(self, name):
+    self.name = name # Instance property
+
+p1 = Person("Emil")
+p2 = Person("Tobias")
+
+print(p1.name)
+print(p2.name)
+print(p1.species)
+print(p2.species) 
